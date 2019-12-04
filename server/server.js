85c92e02c7dfe174a3de3a9225b4a0bbbc108c73
Strider Wilson <Strider@Striders-MacBook-Pro.local>
2019-12-04 09:18:26 -0800

    Test route /api/images/3

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const Sequelize = require('sequelize');
 const { Property, Image } = require('./../database/seed.js');
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,5 +36,4 @@ app.get('/api/images/:propertyId', (req, res) => {
 
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Listening on port ${port}`));
+module.exports = app;
